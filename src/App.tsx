bdbd0a33dfc79029da9d11f9bcc35f1ad96e0035
pyrozhenkovlad <pyrozhenko22vladyslav@gmail.com>
2023-03-15 16:17:02 +0200

    add deleting and saving position logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,15 @@ function App() {
     const localTiles = localStorage.getItem("tiles");
     if (localTiles) {
       setTiles(JSON.parse(localTiles));
-    } /* else {
+    } else {
       localStorage.setItem("tiles", JSON.stringify(tiles));
-    } */
+    }
   }, []);
+
+  useEffect(() => {
+    console.log("tiles", tiles);
+    localStorage.setItem("tiles", JSON.stringify(tiles));
+  }, [tiles]);
 
   const activeHandler = (id: number) => {
     const newTiles = tiles.map((tile) => {
@@ -25,10 +30,20 @@ function App() {
     setTiles(newTiles);
   };
 
+  const returnLastTile = () => {
+    const deletedTile = localStorage.getItem("deletedTile");
+    if (deletedTile) {
+      const newTiles = [...tiles];
+      newTiles.push(JSON.parse(deletedTile));
+      setTiles(newTiles);
+    }
+    localStorage.removeItem("deletedTile");
+  };
+
   return (
     <>
-      <div className="h-screen">
-        <div className="flex flex-col gap-y-4 absolute">
+      <div className="h-screen relative">
+        <div className="flex flex-col gap-y-4">
           {tiles.map((tile) => (
             <Tile
               //@ts-ignore
@@ -45,7 +60,9 @@ function App() {
         </div>
         <div className="grid place-content-center fixed top-[40%] left-[40%]">
           <div className="border border-black grid place-content-center w-80 h-40">
-            <p className="cursor-pointer">Return tile to trading desk</p>
+            <p className="cursor-pointer" onClick={returnLastTile}>
+              Return tile to trading desk
+            </p>
           </div>
         </div>
       </div>
--- src/Components/Tile.tsx
+++ src/Components/Tile.tsx
@@ -7,37 +7,71 @@ interface TileProps {
   id: number;
   size: { width: string; height: string };
   position: { x: number; y: number };
-  setTiles: (position: { x: number; y: number }) => void;
+  setTiles: () => void;
   onClick: (id: number) => void;
 }
 
 const Tile = (props: TileProps) => {
-
   const eventLogger = (e: MouseEvent, data: Object) => {
+    props.onClick(props.id);
     //@ts-ignore
-    console.log('Data X: ', data.x);
+    props.setTiles((prevTiles) => {
+      const index = prevTiles.findIndex((tile: any) => tile.id === props.id);
+      const updatedTile = {
+        ...prevTiles[index],
+        position: {
+          //@ts-ignore
+          x: data.x,
+          //@ts-ignore
+          y: data.y,
+        },
+      };
+      const newTiles = [...prevTiles];
+      newTiles[index] = updatedTile;
+      return newTiles;
+    });
+  };
+
+  const deleteHandler = (e: React.MouseEvent, id: number) => {
+    e.stopPropagation();
     //@ts-ignore
-    console.log('Data Y: ', data.y);
+    props.setTiles((prevTiles) => {
+      const index = prevTiles.findIndex((tile: any) => tile.id === id);
+      const deletedTile = prevTiles[index];
+      localStorage.setItem("deletedTile", JSON.stringify(deletedTile));
+      const newTiles = [...prevTiles];
+      newTiles.splice(index, 1);
+      return newTiles;
+    });
   };
 
   return (
     <>
       <Draggable
-        onStart={()=>{props.onClick(props.id)}}
         //@ts-ignore
-        onStop={(e, data) => eventLogger(e , data)}
+        onStart={(e, data) => eventLogger(e, data)}
+        position={{ x: props.position.x, y: props.position.y }}
+        //@ts-ignore
+        onStop={(e, data) => eventLogger(e, data)}
       >
         <div
           className={`border ${
             props.active ? "border-blue-500" : "border-black"
-          } h-[150px] w-[300px]  ${props.active ? " z-20" : "z-10"} bg-green-100`}
+          } h-[150px] w-[300px]  ${
+            props.active ? " z-20" : "z-10"
+          } bg-gray-100 absolute cursor-pointer`}
         >
           <div
             className={`h-10  flex flex-row border ${
               props.active ? "bg-blue-200 border-b-blue-500" : "border-b-black "
             } ] `}
           >
-            <div className="self-center grid border place-content-center h-4 w-4 cursor-pointer"></div>
+            <div
+              className="self-center grid border ml-1 bg-white place-content-center h-4 w-4 cursor-pointer"
+              onClick={(e) => deleteHandler(e, props.id)}
+            >
+              -
+            </div>
             <div className=" grid place-content-center flex-1">
               {props.title}
             </div>
